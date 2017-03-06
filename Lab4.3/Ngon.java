2fad9eacc8ac6450e3f7698b894dadbda24512dc
Alex <otaku.me@icloud.com>
2017-03-06 09:12:41 -0500

    Completed Ngon Class
    The Ngon class now determines which shape needs to be drawn based on
    number of sides

--- Lab4.3/Ngon.java
+++ Lab4.3/Ngon.java
@@ -1,3 +1,17 @@
 public class Ngon {
-
+	public void draw(){}
+	
+	public int whichShape(int numOfSides) {
+		switch(numOfSides) {
+			case 3:
+				return Triangle;
+				break;
+			case 4:
+				return Parallelogram();
+				break;
+			case 6:
+				return Hexagon();
+				break;
+		}
+	}
 }
