c87ae746d6a206cb58b02b9f4e425b89c7750a2f
Alex <otaku.me@icloud.com>
2017-03-05 21:18:06 -0500

    Edited for testing
    Learned how the program functioned and simplified it to make it clearer
    as a base for the future classes

--- Lab4.3/ShapesTester.java
+++ Lab4.3/ShapesTester.java
@@ -1,22 +1,10 @@
-/* AP Computer Science A
- * Demonstrative Code for how the classes should 
- * interact and run.
- * 
- * This shows a multi-colored octagon.
- * 
- * 
- */
 import java.awt.Color;
 public class ShapesTester {
 	public static void main(String[] args){
-		Color[] colors = {Color.PINK, Color.BLUE, Color.GREEN,
-	        		 	  Color.RED, Color.CYAN, Color.MAGENTA,
-	        		 	  Color.GRAY, Color.ORANGE};
+		int length = 5;
 		Turtle turtle = new Turtle(.5, .5, 0.0);
-	    double angle = 360.0/colors.length;
-	    for (int t = 0; t < colors.length; t++) {
-	    	turtle.setPenColor(colors[(int) (Math.random() * colors.length)]);
-	    	turtle.setPenRadius(Math.random()/100);
+	    double angle = 360.0/length;
+	    for (int t = 0; t < length; t++) {
 	        turtle.turnLeft(angle);
 	        turtle.goForward(.1);
 	        turtle.show();
