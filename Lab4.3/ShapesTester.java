a742b941ae77822a1a277d65c9062cf154272b0b
Alex <otaku.me@icloud.com>
2017-03-06 23:41:01 -0500

    Finalized Parallelogram

--- Lab4.3/ShapesTester.java
+++ Lab4.3/ShapesTester.java
@@ -2,10 +2,11 @@ import java.awt.Color;
 public class ShapesTester {
 	public static void main(String[] args){
 		//testing Parallelogram
-		Coord coord1 = new Coord(1,1);
-		Coord coord2 = new Coord(1,2);
-		Coord coord3 = new Coord(2,2);
-		Coord coord4 = new Coord(2,1);
+		StdDraw.setPenRadius(.01);
+		Coord coord1 = new Coord(.1,.1);
+		Coord coord2 = new Coord(.1,.2);
+		Coord coord3 = new Coord(.2,.2);
+		Coord coord4 = new Coord(.2,.1);
 		Parallelogram.draw(coord1, coord2, coord3, coord4);
 	}	
 }	
