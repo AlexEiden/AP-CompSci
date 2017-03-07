47ccecb979bc070d66fb810fe105f589154d29bc
Alex <otaku.me@icloud.com>
2017-03-06 22:24:35 -0500

    Implementation for drawing a Parallelogram Completed

--- Lab4.3/Parallelogram.java
+++ Lab4.3/Parallelogram.java
@@ -1,3 +1,10 @@
-public class Parallelogram {
-	public draw(){}
+public class Parallelogram extends Ngon {
+	public draw(Coord one, Coord two, Coord three, Coord four){
+		//build checker
+		StdDraw.line(one.getX(), one.getY(), two.getX(), two.getY());		
+		StdDraw.line(two.getX(), two.getY(), three.getX(), three.getY());
+		StdDraw.line(three.getX(), three.getY(), four.getX(), four.getY());
+		StdDraw.line(four.getX(), four.getY(), one.getX(), one.getY());
+
+	}
 }
