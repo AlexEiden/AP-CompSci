8a2d3c6c91a3643de226f4b96907ef7830110ba5
Alex <otaku.me@icloud.com>
2017-03-07 00:49:36 -0500

    Completed Trapezoid Class and Updated Tester

--- Lab4.3/ShapesTester.java
+++ Lab4.3/ShapesTester.java
@@ -8,6 +8,9 @@ public class ShapesTester {
 		Coord coord3 = new Coord(.2,.2);
 		Coord coord4 = new Coord(.1,.2);
 		Parallelogram.draw(coord1, coord2, coord3, coord4);
-
+		//testing Trapezoid
+		coord1.setX(.05); coord2.setX(0.1); coord3.setX(0.1); coord4.setX(.05);
+		coord1.setY(.05); coord2.setY(.05); coord3.setY(0.1); coord4.setY(0.1);
+		Trapezoid.draw(coord1, coord2, coord3, coord4);
 	}	
 }	
