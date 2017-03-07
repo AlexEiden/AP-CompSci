2b04c753d60a0a9ea9ba54b2aadb5dcb2efcb7aa
Alex <otaku.me@icloud.com>
2017-03-06 22:14:32 -0500

    getAngle now obsolete as ignoring Turtle

--- Lab4.3/Ngon.java
+++ Lab4.3/Ngon.java
@@ -1,13 +1,15 @@
 public class Ngon extends Shape {
 	public void draw(){}
 	
-	public double getAngle() {
+	/* public double getAngle(double x, double y) {
 		double angle = (double) Math.toDegrees(Math.atan2(target.y - y, target.x - x));
 	
     	if(angle < 0){
 			 angle += 360;
     	}
    		 return angle;
-	}
+	} */ 
+	
+	//Now obsolete as I am ignoring the Tutle class and drawing straight from StdDraw
 
 }
