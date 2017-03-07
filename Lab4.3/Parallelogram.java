5036d740d415c5c4c7fac5655d565f1968c4283e
Alex <otaku.me@icloud.com>
2017-03-06 23:47:15 -0500

    Updated Parallelogram to be clearer

--- Lab4.3/Parallelogram.java
+++ Lab4.3/Parallelogram.java
@@ -1,10 +1,12 @@
 public class Parallelogram extends Ngon {
 	public static void draw(Coord one, Coord two, Coord three, Coord four){
 		//build checker
-		StdDraw.line(one.getX(), one.getY(), two.getX(), two.getY());		
-		StdDraw.line(two.getX(), two.getY(), three.getX(), three.getY());
-		StdDraw.line(three.getX(), three.getY(), four.getX(), four.getY());
-		StdDraw.line(four.getX(), four.getY(), one.getX(), one.getY());
+		double x1 = one.getX(), x2 = two.getX(), x3 = three.getX(), x4 = four.getX();
+		double y1 = one.getY(), y2 = two.getY(), y3 = three.getY(), y4 = four.getY();
+		StdDraw.line(x1, y1, x2, y2);		
+		StdDraw.line(x2, y2, x3, y3);
+		StdDraw.line(x3, y3, x4, y4);
+		StdDraw.line(x4, y4, x1, y1);
 
 	}
 }
