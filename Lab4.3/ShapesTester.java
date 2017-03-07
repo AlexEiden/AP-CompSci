7384adfc29b36464f5a9ce56f33795f9f1ad7b1a
Alex <otaku.me@icloud.com>
2017-03-07 00:03:20 -0500

    Minor comment edits

--- Lab4.3/ShapesTester.java
+++ Lab4.3/ShapesTester.java
@@ -1,12 +1,13 @@
 import java.awt.Color;
 public class ShapesTester {
 	public static void main(String[] args){
-		//testing Parallelogram
-		StdDraw.setPenRadius(.01);
+		StdDraw.setPenRadius(.01); //setting thickness for all, man does it need to be small..
+		//testing parallelogram
 		Coord coord1 = new Coord(.1,.1);
-		Coord coord2 = new Coord(.1,.2);
+		Coord coord2 = new Coord(.2,.1);
 		Coord coord3 = new Coord(.2,.2);
-		Coord coord4 = new Coord(.2,.1);
+		Coord coord4 = new Coord(.1,.2);
 		Parallelogram.draw(coord1, coord2, coord3, coord4);
+
 	}	
 }	
