f9b1d1e5a06a1bb044d848e761fc27b1fd7b1f0c
Alex <otaku.me@icloud.com>
2017-03-07 01:07:50 -0500

    Adding comments

--- Lab4.3/RegularHexagon.java
+++ Lab4.3/RegularHexagon.java
@@ -1,6 +1,7 @@
 public class RegularHexagon {
 	public static void draw(double length) {
-		Turtle turtle = new Turtle(.75, .75, 0.0);
+		Turtle turtle = new Turtle(.75, .75, 0.0); //using Tutle out of laziness
+													//don't want to calculate coords :(
 	    double angle = 60;
 	    for (int i = 0; i < 6; i++) {
 	        turtle.turnLeft(angle);
