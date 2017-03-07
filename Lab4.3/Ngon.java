b634a8c77098fa4428d7c00b5b174a41c3c8adbe
Alex <otaku.me@icloud.com>
2017-03-06 21:59:34 -0500

    Added a getAngle method in case if I need to use Turtle

--- Lab4.3/Ngon.java
+++ Lab4.3/Ngon.java
@@ -1,4 +1,13 @@
 public class Ngon extends Shape {
 	public void draw(){}
 	
+	public double getAngle() {
+		double angle = (double) Math.toDegrees(Math.atan2(target.y - y, target.x - x));
+	
+    	if(angle < 0){
+			 angle += 360;
+    	}
+   		 return angle;
+	}
+
 }
