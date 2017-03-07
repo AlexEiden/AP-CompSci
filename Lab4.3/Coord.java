abb6ebe45ffd5281fb8cbf132587ec932302932c
Alex <otaku.me@icloud.com>
2017-03-06 21:14:34 -0500

    Cleared Ngon and Created Coord class

---
+++ Lab4.3/Coord.java
@@ -0,0 +1,17 @@
+public class Coord {
+
+	private double x;
+	private double y;
+
+	public Coord(double xPos, double yPos) {
+		x = xPos;
+		y = yPos;
+	}
+
+	public getX() {
+		return x;
+	}
+	public getY() {
+		return y;
+	}
+}
--- Lab4.3/Ngon.java
+++ Lab4.3/Ngon.java
@@ -1,17 +1,4 @@
 public class Ngon extends Shape {
 	public void draw(){}
 	
-	public int whichShape(int numOfSides) {
-		switch(numOfSides) {
-			case 3:
-				return Triangle;
-				break;
-			case 4:
-				return Parallelogram();
-				break;
-			case 6:
-				return Hexagon();
-				break;
-		}
-	}
 }
